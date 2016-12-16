01b2d9a40db8c4b8845bab418c552af29476b46a
d333q <d33q@yandex.ru>
2016-12-16 18:47:17 +0000

    remove info_repositories, added parameter params, remove magic number

--- github_trending.py
+++ github_trending.py
@@ -3,31 +3,25 @@ import json
 from datetime import datetime, date, time, timedelta
 
 
-def get_trending_repositories(top_size):
-    info_repositories = []
+def get_trending_repositories():
     date_week_back = datetime.now() - timedelta(days=7)
-    url = ('https://api.github.com/search/'
-           'repositories?q=created:>={:%Y-%m-%d}&sort='
-           'stars'.format(date_week_back))
-    list_trending_repositories = requests.get(url).json()
+    payload = {
+        'q': ('created:>={:%Y-%m-%d}'.format(date_week_back)), 'sort': 'stars'}
+    list_trending_repositories = requests.get(
+        'https://api.github.com/search/repositories', params=payload).json()
+    return list_trending_repositories
+
+
+def print_info_repositories(list_trending_repositories, top_size):
     for repository in list_trending_repositories['items'][0:top_size]:
-        info_repositories.append({
-                'name': repository.get('name'),
-                'stargazers_count': repository.get('stargazers_count'),
-                'open_issues_count': repository.get('open_issues_count'),
-                'link': repository.get('html_url')})
-    return info_repositories
-
-
-def print_info_repositories(info_repositories):
-    for repository in repositories:
         print('Name: {}, Star: {}, Open issues: {}, Link: {} '.format(
-                    repository['name'],
-                    repository['stargazers_count'],
-                    repository['open_issues_count'],
-                    repository['link']))
+                    repository.get('name'),
+                    repository.get('stargazers_count'),
+                    repository.get('open_issues_count'),
+                    repository.get('html_url')))
 
 
 if __name__ == '__main__':
-    repositories = get_trending_repositories(20)
-    print_info_repositories(repositories)
+    amount_show_repositories = 20
+    repositories = get_trending_repositories()
+    print_info_repositories(repositories, amount_show_repositories)
